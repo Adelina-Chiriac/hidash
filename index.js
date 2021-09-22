4d923cd1868157051afcfb4053b220ef339a3bb4
adelina-chiriac <adelinachiriac@gmail.com>
2021-09-22 18:37:23 +0100

    Refactor the forEach function

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 module.exports = {
     forEach(arr, func) {
-        for (let i = 0; i < arr.length; i++) {
-            const value = arr[i];
-            func(value, i);
+        // for (let i = 0; i < arr.length; i++) {
+        //     const value = arr[i];
+        //     func(value, i);
+        // }
+        for (let index in arr) {
+            func(arr[index], index);
         }
     }
 };
